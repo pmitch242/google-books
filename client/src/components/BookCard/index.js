edba94093a0edf67acb8602e9db95f153b608d6e
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-26 16:58:47 -0400

    got cards to append onto page with relevant information

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import {  } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import './style.css';
 
 class BookCard extends Component {
     render() {
-        return 
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <img src={this.props.image} alt='Book Image'/> 
+                    </Col>
+                    <Col xs={10}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>{this.props.title}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">Written By: {this.props.authors}</Card.Subtitle>
+                                <Card.Text>{this.props.description}</Card.Text>
+                                <Card.Link href={this.props.link}>View</Card.Link>
+                                <Card.Link href="#">Save</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        )
     }
 }
 
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
-import SearchForm from '../../components/SearchForm'
+import { Form, Button } from 'react-bootstrap';
+import './style.css'
+import BookCard from '../../components/BookCard'
+import API from '../../utils/API'
+
 
 class Search extends Component {
+    state = {
+        results: [],
+        search: ''
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ search: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        API.google(this.state.search)
+            .then(res => {
+                this.setState({
+                    search: '',
+                    results: res.data.items,
+                })
+            })
+            console.log(this.state)
+    }
+
     render() {
+        const className = {
+            button: 'button'
+        }
+        let card = this.state.results.map((book) => {
+            let info = book.volumeInfo;
+            return < BookCard 
+            key ={book.id}
+            image={info.imageLinks.thumbnail}
+            title={info.title}
+            authors={info.authors}
+            description={
+                !book.searchInfo?.textSnippet ? 
+                ("No description provided...") : 
+                (book.searchInfo.textSnippet)
+            }
+            link={info.infoLink}
+        />
+        })
+        
         return (
             <div>
                 <Container>
-                    <SearchForm />
+                    <Form>
+                        <Form.Group>
+                            <Form.Control value={this.state.search} onChange={this.handleInputChange} name='search' type="text" placeholder="Search Book" />
+                        </Form.Group>
+                        <Button disabled={!this.state.search} onClick={this.handleSubmit} className={className.button} variant="primary" type="submit">
+                            Search
+                        </Button>
+                    </Form>
+                    {card}
                 </Container>
             </div>
         )
