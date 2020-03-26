859259dde30f0cc3b7fcda9a95171a931f69513d
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-26 08:44:51 -0400

    setting up page

---
+++ client/pages/Saved/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class Saved extends Component {
+
+}
+
+export default Saved;
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
