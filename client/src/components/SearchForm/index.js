8dc8c9208540bca01f1f00e5076c9c67a0d8404f
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-26 14:06:01 -0400

    added google api call as well as set states

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './style.css';
+import API from '../../utils/API'
 
 class SearchForm extends Component {
+    state = {
+        results: [],
+        search: ''
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ search: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        API.google(this.state.search)
+            .then(res => {
+                this.setState({
+                    search: '',
+                    results: res.data.items,
+                })
+                console.log(this.state);
+            })
+    }
+
     render() {
-        let className = {
+        const className = {
             button: 'button'
         }
         return (
-            <div>
+            <Form>
                 <Form.Group>
-                    <Form.Control type="text" placeholder="Search Book" />
+                    <Form.Control value={this.state.search} onChange={this.handleInputChange} name='search' type="text" placeholder="Search Book" />
                 </Form.Group>
-                <Button className={className.button} variant="primary" type="submit">
+                <Button disabled={!this.state.search} onClick={this.handleSubmit} className={className.button} variant="primary" type="submit">
                     Search
                 </Button>
-            </div>
+            </Form>
         )
     }
 }
