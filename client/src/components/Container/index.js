3b5b366807a5dcd5c357e4208e7f5c6588e4e9cc
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-26 11:01:09 -0400

    Initial commit

---
+++ client/src/components/Container/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {  } from 'react-bootstrap';
+import './style.css';
+
+class Container extends Component {
+    render() {
+        return 
+    }
+}
+
+export default Container;
---
+++ client/src/components/Form/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {  } from 'react-bootstrap';
+import './style.css';
+
+class Form extends Component {
+    render() {
+        return 
+    }
+}
+
+export default Form;
