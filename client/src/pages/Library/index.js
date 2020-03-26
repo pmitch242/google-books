4694a2e71b9c2bc924366f0e59a05c3127e92342
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-26 09:15:00 -0400

    added test header

--- client/src/pages/Library/index.js
+++ client/src/pages/Library/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 class Library extends Component {
-
+    render() {
+        return(
+            <h1>In Library</h1>
+        )
+    }
 }
 
 export default Library;
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-
+    render() {
+        return(
+            <h1>In Search</h1>
+        )
+    }
 }
 
 export default Search;
