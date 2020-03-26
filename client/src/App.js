e7a520baa2884afcce60b8e55187bc3a6847dec3
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-26 10:35:00 -0400

    imported Jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbo from './components/Jumbo'
 import Search from './pages/Search';
 import Library from './pages/Library';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <div>
+        <Jumbo />
         <Switch>
           <Route exact path={['/', '/search']}>
             <Search />
