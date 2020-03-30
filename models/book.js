81c884b5388a5cef1c6c64dccdc01e16e1c6b2d8
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-30 15:09:09 -0400

    created models and controllers. also defined routes

--- models/book.js
+++ models/book.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
+  image: { type: String, required: false, default: 'https://via.placeholder.com/150' },
+  link: { type: String, required: true },
   synopsis: String,
   date: { type: Date, default: Date.now }
 });
