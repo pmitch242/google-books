82ee017c036d07dd85e4a1fe2128d352d48b9af3
Phill <phillicia.mitchell21@gmail.com>
2020-05-30 23:46:00 -0400

    styled search bar

--- client/src/components/search-bar/SearchBar.jsx
+++ client/src/components/search-bar/SearchBar.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 import './search-bar.css';
 
 export default function SearchBar() {
+    const style = {
+        searchBarDiv: {
+
+        },
+        searchBar: {
+            border: 'none',
+            borderRadius: '30px',
+            backgroundColor: '#f3f5f9',
+            padding: '10px',
+            boxShadow: '0px 5px 10px 0px rgba(0,0,0,0.12)',
+
+
+        },
+        searchBarInput: {
+            border: 'none',
+            backgroundColor: 'transparent',
+            width: '86%'
+        },
+        icon: {
+            marginRight: '15px',
+            color: '#1a4cb1'
+        }
+    }
     return (
-        <Form>
-            <Form.Group>
-                <Form.Control name='search' type="text" placeholder="Search Book" />
-            </Form.Group>        
-        </Form>
+
+        <div className='search-bar-div'>
+            <form style={style.searchBar} className='search-bar'>
+                <FontAwesomeIcon icon={faSearch} style={style.icon} className='search-bar-icon'/>
+                <input type="text" id="search-input" name="search-input" placeholder='Search by author, title...' style={style.searchBarInput}/>
+            </form>
+        </div>
     )
 }
