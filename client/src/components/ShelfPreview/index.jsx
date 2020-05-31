bb4655f5769145bcc91867bdc272e68280e71004
Phill <phillicia.mitchell21@gmail.com>
2020-05-31 16:52:27 -0400

    styled header

--- client/src/components/ShelfPreview/index.jsx
+++ client/src/components/ShelfPreview/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 import './shelf-preview.css';
 
 const ShelfPreview = () => {
@@ -11,14 +14,30 @@ const ShelfPreview = () => {
             padding: '15px 20px',
             boxShadow: '0px 5px 10px 0px rgba(0,0,0,0.12)',
         },
-
+        h3: {
+            marginBottom: '0px',
+        },
+        viewAllDiv: {
+            height: 'fit-content',
+            marginTop: 'auto',
+        },
+        span: {
+            marginRight: '10px',
+        },
+        icon: {
+            fontSize: 'x-small',
+            marginBottom: '1px',
+        }
     }
 
     return (
         <div style={style.container} className='shelf-preview-div'>
             <div className='title-link-div'>
-                <h2>Shelf</h2>
-                <span>View all</span>
+                <h3 style={style.h3}>Shelf</h3>
+                <div style={style.viewAllDiv}>
+                    <span style={style.span}>View all</span>
+                    <FontAwesomeIcon icon={faChevronRight} style={style.icon}/>
+                </div>
             </div>
         </div>
     )
