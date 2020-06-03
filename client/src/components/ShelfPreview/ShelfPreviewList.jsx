6616e36e88a5b84eb1449baa941a90fd367cbe80
Phill <phillicia.mitchell21@gmail.com>
2020-06-03 17:58:25 -0400

    created imaged card and imported it to ShelfPreviewList component

--- client/src/components/ShelfPreview/ShelfPreviewList.jsx
+++ client/src/components/ShelfPreview/ShelfPreviewList.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
+import ImageCard from '../cards/ImageCard';
+
 import './shelf-preview.css';
 
 const ShelfPreviewList = () => {
     const style = {
-        img: {
-            width: 'inherit',
-            height: 'inherit',
-            borderRadius: '20px',
-        },
-        imgDiv: {
-            width: '125px',
-            maxWidth: '400px',
-            height: 'auto',
-        },
         scrollDiv: {
             marginLeft: '-35px',
             paddingLeft: '35px',
@@ -23,55 +15,9 @@ const ShelfPreviewList = () => {
 
     return (
         <div className='horizontal-scroll' style={style.scrollDiv}>
-            <div className='image-card' style={style.imgDiv}>
-                <img
-                    alt='book'
-                    src='https://i.pinimg.com/originals/1e/c5/df/1ec5df963765d4bcf151467c99d1dae7.jpg'
-                    style={style.img}
-                />
-            </div>
-
-            <div className='image-card' style={style.imgDiv}>
-                <img
-                    alt='book'
-                    src='https://i.pinimg.com/originals/1e/c5/df/1ec5df963765d4bcf151467c99d1dae7.jpg'
-                    style={style.img}
-                />
-            </div>
-
-            <div className='image-card' style={style.imgDiv}>
-                <img
-                    alt='book'
-                    src='https://i.pinimg.com/originals/1e/c5/df/1ec5df963765d4bcf151467c99d1dae7.jpg'
-                    style={style.img}
-                />
-            </div>
-
-            <div className='image-card' style={style.imgDiv}>
-                <img
-                    alt='book'
-                    src='https://i.pinimg.com/originals/1e/c5/df/1ec5df963765d4bcf151467c99d1dae7.jpg'
-                    style={style.img}
-                />
-            </div>
-
-
-            <div className='image-card' style={style.imgDiv}>
-                <img
-                    alt='book'
-                    src='https://i.pinimg.com/originals/1e/c5/df/1ec5df963765d4bcf151467c99d1dae7.jpg'
-                    style={style.img}
-                />
-            </div>
-
-
-            <div className='image-card' style={style.imgDiv}>
-                <img
-                    alt='book'
-                    src='https://i.pinimg.com/originals/1e/c5/df/1ec5df963765d4bcf151467c99d1dae7.jpg'
-                    style={style.img}
-                />
-            </div>
+            <ImageCard
+            bookTitle='Finding Moana'
+            imageLink='https://i.pinimg.com/originals/1e/c5/df/1ec5df963765d4bcf151467c99d1dae7.jpg' />
         </div>
     )
 }
--- client/src/components/cards/ImageCard.jsx
+++ client/src/components/cards/ImageCard.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const ImageCard = () => {
-    return
+const ImageCard = (props) => {
+    const style = {
+        img: {
+            width: 'inherit',
+            height: 'inherit',
+            borderRadius: '20px',
+        },
+        imgDiv: {
+            width: '125px',
+            maxWidth: '400px',
+            height: 'auto',
+        },
+    }
+
+    return (
+        <div className='image-card' style={style.imgDiv}>
+            <img
+                alt={props.bookTitle}
+                src={props.imageLink}
+                style={style.img}
+            />
+        </div>
+    )
 }
 
 export default ImageCard;
