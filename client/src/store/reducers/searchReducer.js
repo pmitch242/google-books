51d877d75acb9862a2bde80b041cbbcf39b4d02d
Phill <phillicia.mitchell21@gmail.com>
2020-06-15 21:58:58 -0400

    added some dummie data to test reducer

--- client/src/store/reducers/searchReducer.js
+++ client/src/store/reducers/searchReducer.js
@@ -1,4 +1,12 @@
-const initState = {}
+const initState = {
+    searchHistory: [
+        {id: '1', search: '50 shades of grey'},
+        {id: '2', search: 'so you want to talk about race'},
+        {id: '3', search: 'between the world and me'},
+        {id: '4', search: 'monopoly'},
+        {id: '5', search: 'renee'},
+    ]
+}
 
 const searchReducer = (state = initState, action) => {
     return state;
