f8d73aeaa28237aabd125a27a06b2d1b03665f1c
Phill <phillicia.mitchell21@gmail.com>
2020-06-17 16:11:37 -0400

    Initial commit

---
+++ client/src/components/BottomNav/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+
+const BottomNav = () => {
+    return (
+        <Navbar>
+            <Navbar.Brand href="/">Testing Nav</Navbar.Brand>
+        </Navbar>
+    )
+}
+
+export default BottomNav;
