cb962fbfa59865be5c7728eee44c3df11c248b65
Phill <phillicia.mitchell21@gmail.com>
2020-06-18 16:50:00 -0400

    added dummy info

--- client/src/store/reducers/searchReducer.js
+++ client/src/store/reducers/searchReducer.js
@@ -1,12 +1,334 @@
 const initState = {
     searches: [
-        {id: '1', search: '50 shades of grey'},
-        {id: '2', search: 'so you want to talk about race'},
-        {id: '3', search: 'between the world and me'},
-        {id: '4', search: 'monopoly'},
-        {id: '5', search: 'renee'},
+        { id: '1', search: '50 shades of grey' },
+        { id: '2', search: 'so you want to talk about race' },
+        { id: '3', search: 'between the world and me' },
+        { id: '4', search: 'monopoly' },
+        { id: '5', search: 'renee' },
     ],
-    searchResults: []
+    searchResults: [
+        {
+            "kind": "books#volume",
+            "id": "MMhpS90HrukC",
+            "etag": "EAVhADTEOB4",
+            "selfLink": "https://www.googleapis.com/books/v1/volumes/MMhpS90HrukC",
+            "volumeInfo": {
+                "title": "Fifty Shades of Grey",
+                "authors": [
+                    "E. L. James"
+                ],
+                "publisher": "Vintage",
+                "publishedDate": "2012",
+                "description": "When Anastasia Steele, a young literature student, interviews wealthy young entrepreneur Christian Grey for her campus magazine, their initial meeting introduces Anastasia to an exciting new world that will change them both forever.",
+                "industryIdentifiers": [
+                    {
+                        "type": "ISBN_13",
+                        "identifier": "9780345803481"
+                    },
+                    {
+                        "type": "ISBN_10",
+                        "identifier": "0345803485"
+                    }
+                ],
+                "readingModes": {
+                    "text": false,
+                    "image": false
+                },
+                "pageCount": 514,
+                "printType": "BOOK",
+                "categories": [
+                    "Fiction"
+                ],
+                "averageRating": 3,
+                "ratingsCount": 558,
+                "maturityRating": "MATURE",
+                "allowAnonLogging": false,
+                "contentVersion": "0.8.1.0.preview.0",
+                "panelizationSummary": {
+                    "containsEpubBubbles": false,
+                    "containsImageBubbles": false
+                },
+                "imageLinks": {
+                    "smallThumbnail": "http://books.google.com/books/content?id=MMhpS90HrukC&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                    "thumbnail": "http://books.google.com/books/content?id=MMhpS90HrukC&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                },
+                "language": "en",
+                "previewLink": "http://books.google.com/books?id=MMhpS90HrukC&dq=50+shades+of+grey&hl=&cd=1&source=gbs_api",
+                "infoLink": "http://books.google.com/books?id=MMhpS90HrukC&dq=50+shades+of+grey&hl=&source=gbs_api",
+                "canonicalVolumeLink": "https://books.google.com/books/about/Fifty_Shades_of_Grey.html?hl=&id=MMhpS90HrukC"
+            },
+            "saleInfo": {
+                "country": "US",
+                "saleability": "NOT_FOR_SALE",
+                "isEbook": false
+            },
+            "accessInfo": {
+                "country": "US",
+                "viewability": "NO_PAGES",
+                "embeddable": false,
+                "publicDomain": false,
+                "textToSpeechPermission": "ALLOWED_FOR_ACCESSIBILITY",
+                "epub": {
+                    "isAvailable": false
+                },
+                "pdf": {
+                    "isAvailable": false
+                },
+                "webReaderLink": "http://play.google.com/books/reader?id=MMhpS90HrukC&hl=&printsec=frontcover&source=gbs_api",
+                "accessViewStatus": "NONE",
+                "quoteSharingAllowed": false
+            },
+            "searchInfo": {
+                "textSnippet": "When Anastasia Steele, a young literature student, interviews wealthy young entrepreneur Christian Grey for her campus magazine, their initial meeting introduces Anastasia to an exciting new world that will change them both forever."
+            }
+        },
+        {
+            "kind": "books#volume",
+            "id": "tHaEDwAAQBAJ",
+            "etag": "o2bUm4h302s",
+            "selfLink": "https://www.googleapis.com/books/v1/volumes/tHaEDwAAQBAJ",
+            "volumeInfo": {
+                "title": "The Mister",
+                "authors": [
+                    "E L James"
+                ],
+                "publisher": "Vintage",
+                "publishedDate": "2019-04-16",
+                "description": "The passionate new romance from E L James, author of the phenomenal #1 bestselling Fifty Shades Trilogy London, 2019. Life has been easy for Maxim Trevelyan. With his good looks, aristocratic connections, and money, he’s never had to work and he’s rarely slept alone. But all that changes when tragedy strikes and Maxim inherits his family’s noble title, wealth, and estates, and all the responsibility that entails. It’s a role he’s not prepared for and one that he struggles to face. But his biggest challenge is fighting his desire for an unexpected, enigmatic young woman who’s recently arrived in England, possessing little more than a dangerous and troublesome past. Reticent, beautiful, and musically gifted, she’s an alluring mystery, and Maxim’s longing for her deepens into a passion that he’s never experienced and dares not name. Just who is Alessia Demachi? Can Maxim protect her from the malevolence that threatens her? And what will she do when she learns that he’s been hiding secrets of his own? From the heart of London through wild, rural Cornwall to the bleak, forbidding beauty of the Balkans, The Mister is a roller-coaster ride of danger and desire that leaves the reader breathless to the very last page.",
+                "industryIdentifiers": [
+                    {
+                        "type": "ISBN_13",
+                        "identifier": "9781984898333"
+                    },
+                    {
+                        "type": "ISBN_10",
+                        "identifier": "1984898337"
+                    }
+                ],
+                "readingModes": {
+                    "text": true,
+                    "image": false
+                },
+                "pageCount": 512,
+                "printType": "BOOK",
+                "categories": [
+                    "Fiction"
+                ],
+                "averageRating": 3.5,
+                "ratingsCount": 12,
+                "maturityRating": "NOT_MATURE",
+                "allowAnonLogging": true,
+                "contentVersion": "1.5.5.0.preview.2",
+                "panelizationSummary": {
+                    "containsEpubBubbles": false,
+                    "containsImageBubbles": false
+                },
+                "imageLinks": {
+                    "smallThumbnail": "http://books.google.com/books/content?id=tHaEDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                    "thumbnail": "http://books.google.com/books/content?id=tHaEDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+                },
+                "language": "en",
+                "previewLink": "http://books.google.com/books?id=tHaEDwAAQBAJ&printsec=frontcover&dq=50+shades+of+grey&hl=&cd=2&source=gbs_api",
+                "infoLink": "https://play.google.com/store/books/details?id=tHaEDwAAQBAJ&source=gbs_api",
+                "canonicalVolumeLink": "https://play.google.com/store/books/details?id=tHaEDwAAQBAJ"
+            },
+            "saleInfo": {
+                "country": "US",
+                "saleability": "FOR_SALE",
+                "isEbook": true,
+                "listPrice": {
+                    "amount": 9.99,
+                    "currencyCode": "USD"
+                },
+                "retailPrice": {
+                    "amount": 9.99,
+                    "currencyCode": "USD"
+                },
+                "buyLink": "https://play.google.com/store/books/details?id=tHaEDwAAQBAJ&rdid=book-tHaEDwAAQBAJ&rdot=1&source=gbs_api",
+                "offers": [
+                    {
+                        "finskyOfferType": 1,
+                        "listPrice": {
+                            "amountInMicros": 9990000,
+                            "currencyCode": "USD"
+                        },
+                        "retailPrice": {
+                            "amountInMicros": 9990000,
+                            "currencyCode": "USD"
+                        },
+                        "giftable": true
+                    }
+                ]
+            },
+            "accessInfo": {
+                "country": "US",
+                "viewability": "PARTIAL",
+                "embeddable": true,
+                "publicDomain": false,
+                "textToSpeechPermission": "ALLOWED_FOR_ACCESSIBILITY",
+                "epub": {
+                    "isAvailable": true,
+                    "acsTokenLink": "http://books.google.com/books/download/The_Mister-sample-epub.acsm?id=tHaEDwAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+                },
+                "pdf": {
+                    "isAvailable": false
+                },
+                "webReaderLink": "http://play.google.com/books/reader?id=tHaEDwAAQBAJ&hl=&printsec=frontcover&source=gbs_api",
+                "accessViewStatus": "SAMPLE",
+                "quoteSharingAllowed": false
+            },
+            "searchInfo": {
+                "textSnippet": "From the heart of London through wild, rural Cornwall to the bleak, forbidding beauty of the Balkans, The Mister is a roller-coaster ride of danger and desire that leaves the reader breathless to the very last page."
+            }
+        },
+        {
+            "kind": "books#volume",
+            "id": "Avo_A1Z9CcoC",
+            "etag": "wYKKKeMgUCo",
+            "selfLink": "https://www.googleapis.com/books/v1/volumes/Avo_A1Z9CcoC",
+            "volumeInfo": {
+                "title": "Fifty Writers on Fifty Shades of Grey",
+                "authors": [
+                    "Lori Perkins"
+                ],
+                "publisher": "BenBella Books",
+                "publishedDate": "2012",
+                "description": "Fifty writers, ranging from romance authors to adult entertainers, provide observations about the popular novel, including its origins and influence on popular culture.",
+                "industryIdentifiers": [
+                    {
+                        "type": "ISBN_13",
+                        "identifier": "9781937856427"
+                    },
+                    {
+                        "type": "ISBN_10",
+                        "identifier": "1937856429"
+                    }
+                ],
+                "readingModes": {
+                    "text": false,
+                    "image": false
+                },
+                "pageCount": 373,
+                "printType": "BOOK",
+                "categories": [
+                    "Literary Collections"
+                ],
+                "averageRating": 3,
+                "ratingsCount": 2,
+                "maturityRating": "MATURE",
+                "allowAnonLogging": false,
+                "contentVersion": "0.1.0.0.preview.0",
+                "panelizationSummary": {
+                    "containsEpubBubbles": false,
+                    "containsImageBubbles": false
+                },
+                "imageLinks": {
+                    "smallThumbnail": "http://books.google.com/books/content?id=Avo_A1Z9CcoC&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                    "thumbnail": "http://books.google.com/books/content?id=Avo_A1Z9CcoC&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                },
+                "language": "en",
+                "previewLink": "http://books.google.com/books?id=Avo_A1Z9CcoC&dq=50+shades+of+grey&hl=&cd=3&source=gbs_api",
+                "infoLink": "http://books.google.com/books?id=Avo_A1Z9CcoC&dq=50+shades+of+grey&hl=&source=gbs_api",
+                "canonicalVolumeLink": "https://books.google.com/books/about/Fifty_Writers_on_Fifty_Shades_of_Grey.html?hl=&id=Avo_A1Z9CcoC"
+            },
+            "saleInfo": {
+                "country": "US",
+                "saleability": "NOT_FOR_SALE",
+                "isEbook": false
+            },
+            "accessInfo": {
+                "country": "US",
+                "viewability": "NO_PAGES",
+                "embeddable": false,
+                "publicDomain": false,
+                "textToSpeechPermission": "ALLOWED",
+                "epub": {
+                    "isAvailable": false
+                },
+                "pdf": {
+                    "isAvailable": false
+                },
+                "webReaderLink": "http://play.google.com/books/reader?id=Avo_A1Z9CcoC&hl=&printsec=frontcover&source=gbs_api",
+                "accessViewStatus": "NONE",
+                "quoteSharingAllowed": false
+            },
+            "searchInfo": {
+                "textSnippet": "Fifty writers, ranging from romance authors to adult entertainers, provide observations about the popular novel, including its origins and influence on popular culture."
+            }
+        },
+        {
+            "kind": "books#volume",
+            "id": "27H-sgEACAAJ",
+            "etag": "nM2ca7NJ76A",
+            "selfLink": "https://www.googleapis.com/books/v1/volumes/27H-sgEACAAJ",
+            "volumeInfo": {
+                "title": "Grey",
+                "subtitle": "Fifty Shades of Grey As Told by Christian",
+                "authors": [
+                    "E. L. James"
+                ],
+                "publisher": "Arrow",
+                "publishedDate": "2015-06-18",
+                "description": "In Christianâe(tm)s own words, and through his thoughts, reflections, and dreams, E L James offers a fresh perspective on the love story that has enthralled millions of readers around the world. CHRISTIAN GREY exercises control in all things; his world is neat, disciplined, and utterly empty âe\" until the day that Anastasia Steele falls into his office, in a tangle of shapely limbs and tumbling brown hair. He tries to forget her, but instead is swept up in a storm of emotion he cannot comprehend and cannot resist. Unlike any woman he has known before, shy, unworldly Ana seems to see right through him âe\" past the business prodigy and the penthouse lifestyle to Christianâe(tm)s cold, wounded heart. Will being with Ana dispel the horrors of his childhood that haunt Christian every night? Or will his dark sexual desires, his compulsion to control, and the self-loathing that fills his soul drive this girl away and destroy the fragile hope she offers him?",
+                "industryIdentifiers": [
+                    {
+                        "type": "ISBN_10",
+                        "identifier": "1784753254"
+                    },
+                    {
+                        "type": "ISBN_13",
+                        "identifier": "9781784753252"
+                    }
+                ],
+                "readingModes": {
+                    "text": false,
+                    "image": false
+                },
+                "pageCount": 576,
+                "printType": "BOOK",
+                "categories": [
+                    "Fiction"
+                ],
+                "averageRating": 3,
+                "ratingsCount": 31,
+                "maturityRating": "NOT_MATURE",
+                "allowAnonLogging": false,
+                "contentVersion": "preview-1.0.0",
+                "imageLinks": {
+                    "smallThumbnail": "http://books.google.com/books/content?id=27H-sgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                    "thumbnail": "http://books.google.com/books/content?id=27H-sgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                },
+                "language": "en",
+                "previewLink": "http://books.google.com/books?id=27H-sgEACAAJ&dq=50+shades+of+grey&hl=&cd=4&source=gbs_api",
+                "infoLink": "http://books.google.com/books?id=27H-sgEACAAJ&dq=50+shades+of+grey&hl=&source=gbs_api",
+                "canonicalVolumeLink": "https://books.google.com/books/about/Grey.html?hl=&id=27H-sgEACAAJ"
+            },
+            "saleInfo": {
+                "country": "US",
+                "saleability": "NOT_FOR_SALE",
+                "isEbook": false
+            },
+            "accessInfo": {
+                "country": "US",
+                "viewability": "NO_PAGES",
+                "embeddable": false,
+                "publicDomain": false,
+                "textToSpeechPermission": "ALLOWED",
+                "epub": {
+                    "isAvailable": false
+                },
+                "pdf": {
+                    "isAvailable": false
+                },
+                "webReaderLink": "http://play.google.com/books/reader?id=27H-sgEACAAJ&hl=&printsec=frontcover&source=gbs_api",
+                "accessViewStatus": "NONE",
+                "quoteSharingAllowed": false
+            },
+            "searchInfo": {
+                "textSnippet": "In Christian&#39;s own words, and through his thoughts, reflections, and dreams, E L James offers a fresh perspective on the love story that has enthralled millions of readers around the world."
+            }
+        },
+    ]
 }
 
 const searchReducer = (state = initState, action) => {
